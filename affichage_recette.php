48718a45befd76e4453e25219466f97f8428b10c
clement guillemot <guillemot.clement35@gmail.com>
2024-01-04 20:03:20 +0100

    V1 exercice affichage de recette

---
+++ affichage_recette.php
@@ -0,0 +1,46 @@
+<?php
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : des flageolets !',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+];
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        foreach($recipes as $key){
+            if($key["is_enabled"] === true){
+                echo "<h2>".$key['title']."</h2>";
+                echo "<p>".$key['recipe']."</p>";
+                echo "<p><em>".$key['author']."</em></p>";
+                echo "<hr>";
+            }else{
+                echo "";
+            }
+        }
+    ?>
+</body>
+</html>
